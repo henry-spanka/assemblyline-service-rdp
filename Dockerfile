274fb9e33fb73b4d881cd3ab252d7d4a7f7fbd3f
Henry Spanka <henry@spanka.eu>
2025-12-30 12:18:39 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM cccs/assemblyline-v4-service-base:stable
+
+# Python path to the service class from your service directory
+ENV SERVICE_PATH result_rdp.Rdp
+
+# Install any service dependencies here
+# For example: RUN apt-get update && apt-get install -y libyaml-dev
+#
+#              COPY requirements.txt requirements.txt
+#              RUN pip install --no-cache-dir --user --requirement requirements.txt && rm -rf ~/.cache/pip
+
+# Switch to assemblyline user
+USER assemblyline
+
+# Copy Sample service code
+WORKDIR /opt/al_service
+COPY . .
---
+++ result_rdp.py
@@ -0,0 +1,46 @@
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.request import ServiceRequest
+from assemblyline_v4_service.common.result import Result, ResultSection, BODY_FORMAT
+
+class Rdp(ServiceBase):
+    def __init__(self, config=None):
+        super(Rdp, self).__init__(config)
+
+    def start(self):
+        # ==================================================================
+        # Startup actions:
+        #   Your service might have to do some warming up on startup to make things faster
+        # ==================================================================
+
+        self.log.info(f"start() from {self.service_attributes.name} service called")
+
+    def execute(self, request: ServiceRequest) -> None:
+        # ==================================================================
+        # Execute a request:
+        #   Every time your service receives a new file to scan, the execute function is called.
+        #   This is where you should execute your processing code.
+        #   For this example, we will only generate results ...
+        # ==================================================================
+
+        rdpUrl = self.config.get('rdpUrl', '').strip()
+
+        if not rdpUrl:
+            self.log.warning("No RDP URL configured, skipping RDP result generation.")
+            return
+
+        # 1. Create a result object where all the result sections will be saved to
+        result = Result()
+
+        # 2. Create a section to be displayed for this result
+        text_section = ResultSection('CAPEv2 RDP', body_format=BODY_FORMAT.URL,
+                                     body=json.dumps({"name": "RDP File", "url": rdpUrl}))
+
+        # 2.1. Add lines to your section
+        text_section.add_line("This is a line displayed in the body of the section")
+
+
+        # 3. Make sure you add your section to the result
+        result.add_section(text_section)
+
+        # 4. Wrap-up: Save your result object back into the request
+        request.result = result
